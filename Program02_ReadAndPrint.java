495ad866d4f2ea1e015f9c61d9cdc4a6c41c1f75
parthrajwar-gif <parth.rajwar@gmail.com>
2026-04-20 12:34:21 +0530

    Update Program02_ReadAndPrint.java

--- Program02_ReadAndPrint.java
+++ Program02_ReadAndPrint.java
@@ -4,19 +4,15 @@ public class Program02_ReadAndPrint {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // take an integer from user
         System.out.print("Enter an integer: ");
         int num = sc.nextInt();
 
-        // take a float
         System.out.print("Enter a float: ");
         float f = sc.nextFloat();
 
-        // take a single character
         System.out.print("Enter a character: ");
         char ch = sc.next().charAt(0);
 
-        // now print whatever user entered
         System.out.println("You entered integer: " + num);
         System.out.println("You entered float: " + f);
         System.out.println("You entered character: " + ch);
